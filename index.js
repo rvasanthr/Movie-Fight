b718a69a153703bcdcebb2cb1d6df47d3123a75f
jolly <vasanth_r@outlook.com>
2023-02-19 19:17:41 +0530

    Adds search functionality based on input event trigger. At index.html: * Updates UI with a text input. At index.js: * Adds dom link for that UI item. * Adds functionality for event trigger to access omdbapi.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
-        params: { apikey: 'd9835cc5', i: 'tt0848228' }
+        params: { apikey: 'd9835cc5', s: searchTerm }
     });
     console.log(response.data);
 }
 // Invoke Function
-fetchData();
+// fetchData();
+// Dom access for input text
+const search = document.querySelector('input');
+search.addEventListener('input', (event) => {
+    fetchData(search.value);
+});
