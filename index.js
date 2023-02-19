fc1186c1c89aa70e893cda6d07e9eaac39a42534
jolly <vasanth_r@outlook.com>
2023-02-19 18:15:02 +0530

    Updates index.js: * Updates axios request to search for movies based on movie id.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetchData = async () => {
     const response = await axios.get('http://www.omdbapi.com/', {
-        params: { apikey: 'd9835cc5', s: 'avengers' }
+        params: { apikey: 'd9835cc5', i: 'tt0848228' }
     });
     console.log(response.data);
 }
